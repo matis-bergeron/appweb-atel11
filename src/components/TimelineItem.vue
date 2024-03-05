55633880c161029c41b6a991bf950a5f859dd134
Mat44772 <bergeronmatis@gmail.com>
2024-03-05 15:26:16 -0500

    first commit

---
+++ src/components/TimelineItem.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { TimelinePost } from '../posts'
+
+    defineProps<{
+    post: TimelinePost
+    }>()
+
+</script>
+<template>
+
+    <li>
+    <span>{{ post.title }}</span>&nbsp;|&nbsp;
+    <span>{{ post.created.toFormat("d MMM") }}</span>
+    </li>
+
+</template>
+<style></style>
